36b3c7977a6d7f3d7aca96cf46b98bd882457f39
Luis Ramirez <luis@leeto.co>
2025-05-07 22:55:26 +0200

    feat(nvim): change colors for Git Diff

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,12 @@ return {
     "Mofiqul/dracula.nvim",
     config = function()
       vim.cmd.colorscheme("dracula")
+
+      -- Cambios personalizados para los colores de diff
+      vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#303030", fg = "#aaffaa" })
+      vim.api.nvim_set_hl(0, "DiffChange", { bg = "#3a3a3a", fg = "#aaaaff" })
+      vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#3a2a2a", fg = "#ffaaaa" })
+      vim.api.nvim_set_hl(0, "DiffText", { bg = "#44475a", fg = "#ffffff" })
     end,
   },
 }
